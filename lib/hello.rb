68079cc50ec4ac2ce8b24521f88fda96195912c6
Mahmoud Agamy <agamyv@gmail.com>
2018-11-16 23:15:23 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,11 @@
-def hello_t
-
+def hello_t(array)
+  if block_given?
+    array.each { |name| yield(name) }  
+  else
+    puts "Hey! No block was given!"
+  end
 end
 
 # call your method here!
+# by me Mahmoud Agamy "I don't get why calling the method is required here?"" 
 
